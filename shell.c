2b4b2919eb666d2f61900abfce343591296c92cd
Nicholas <nik@nik.local>
2018-05-17 23:50:32 +0200

    I don't remember changees

--- shell.c
+++ shell.c
@@ -21,25 +21,35 @@
 //ARGUMENTS CODE DEFINITION
 //starting with S --> single hyphen arguments i.e. -o
 //starting with L --> double hyphen arguments i.e. --outfile
-//divided into many argument classes
+//divided into many argument classes for keep the possibility to add other arguments
 #define S_ARG_STDOUTFILE 10
 #define L_ARG_STDOUTFILE 1000
 #define S_ARG_STDERRFILE 20
 #define L_ARG_STDERRFILE 2000
 #define S_ARG_MAXLEN 30
 #define L_ARG_MAXLEN 3000
-
+//for not defined arguments
 #define ARG_CODE_ERROR -1
-
+//commands expression buffer size
 #define CMD_EXP_BUFF_SIZE 512
 
+typedef int argcode_t;
+//argument type
+typedef struct{
+	argcode_t arg_code;//the argument's specific code for identification
+	void *arg_value;//based on the argcode, this will be allocated for an int, char*...
+} arg_t;
+
+//function for keep the main logic clean and because it can be called multiple times
 void printUsage( char* _prog_name ){
 	printf("usage: %s [-o | --outfile <file>] [-e | --errfile <file>] [-m | --maxlen <value>]\n	", _prog_name);
 	exit(EXIT_SUCCESS);
 }
 
 //function to dinamically return arguments codes
-int getcode(char *_arg){
+//matches the string with every argument
+//returns error if the string doesn't match any argument
+argcode_t getcode(char *_arg){
 	if(strcmp(_arg, "-o\0") == 0){ return S_ARG_STDOUTFILE; }
 	if(strcmp(_arg, "--outfile\0") == 0){ return L_ARG_STDOUTFILE; }
 	if(strcmp(_arg, "-e\0") == 0){ return S_ARG_STDERRFILE; }
@@ -61,21 +71,33 @@ int main(int argc, char **argv){
 		exit(EXIT_SUCCESS);
 	}
 
-	int curr_arg; //for loop argument counter
-	char *t_arg; //temporary argument holder
-	int arg_code; //argument code holder
 
+	//assuming the worst case, arguments are all on/off switches (without a value)
+	//except one that for sure has to hold an output file name
+	//so we allocate argc-1 positions in the vector (the first is always the executable name)
+	arg_t args[argc-1]; //vector for saving parameters code and value
+	int curr_argv; //for loop argument counter (for accessing argv)
+	int curr_args; //for loop argument counter (for accessing args)
+	char *t_arg; //temporary argument holder
+	argcode_t arg_code; //argument code holder
+
+	/*
 	//arguments variables
 	char *stdout_filepath;
 	char *stderr_filepath;
 	int max_len;
+	*/
 
-	curr_arg = 1;//argv[0] always executable name
+	curr_argv = 1;//argv[0] always executable name
+	curr_args = 0;
 
-	while( curr_arg < argc ){
+	//cycle to parse and elaborate all arguments
+	while( curr_argv < argc ){
 
-		t_arg = argv[curr_arg]; //catching actual parameter
+		t_arg = argv[curr_argv]; //catching actual parameter
 		arg_code = getcode(t_arg); //catching actual parameter's code
+		char *str_value;
+		int *int_value;
 
 		printf("t_arg: %s\n", t_arg);
 		printf("arg_code: %i\n", arg_code);
@@ -83,26 +105,44 @@ int main(int argc, char **argv){
 		//based on the argument's code we know where/how to save the value
 		switch(arg_code){
 
-
 			case L_ARG_STDOUTFILE:
 			case S_ARG_STDOUTFILE:
-				curr_arg++; //next arg to catch the value
-				stdout_filepath = argv[curr_arg]; //catching the value
-				printf("stdout filepath: %s\n", stdout_filepath);
+				curr_argv++; //next arg to catch the value
+				args[curr_args].arg_code = arg_code;
+				str_value = malloc( strlen(argv[curr_argv]) + 1 );
+				printf("before strcpy....\n");
+				strcpy(str_value, argv[curr_argv]);
+				printf("str_value: %s\n", str_value);
+				args[curr_args].arg_value = str_value;
+				printf("arg_value: %s\n", args[curr_args].arg_value);
+
+				str_value = NULL;
+				curr_args++;
 				break;
+
 
 			case L_ARG_STDERRFILE:
 			case S_ARG_STDERRFILE:
-				curr_arg++;
-				stderr_filepath = argv[curr_arg];
-				printf("stderr filepath: %s\n", stderr_filepath);
+				curr_argv++; //next arg to catch the value
+				args[curr_args].arg_code = arg_code;
+				str_value = malloc( strlen(argv[curr_argv]) + 1 );
+				strcpy(str_value, argv[curr_argv]);
+				args[curr_args].arg_value = str_value;
+
+				str_value = NULL;
+				curr_args++;
 				break;
 
 			case L_ARG_MAXLEN:
 			case S_ARG_MAXLEN:
-				curr_arg++;
-				max_len = atoi(argv[curr_arg]);
-				printf("max_len: %i\n", max_len);
+				curr_argv++; //next arg to catch the value
+				args[curr_args].arg_code = arg_code;
+				int_value = malloc( sizeof(int) );
+				*int_value = atoi(argv[curr_argv]);
+				args[curr_args].arg_value = int_value;
+
+				int_value = NULL;
+				curr_args++;
 				break;
 
 			case ARG_CODE_ERROR:
@@ -117,13 +157,27 @@ int main(int argc, char **argv){
 				break;
 		}
 
-		curr_arg++; //in every case we advance to next arg
+		curr_argv++; //in every case we advance to next arg
 	}
 
+	/*
 	printf("outfile: %s\n", stdout_filepath);
 	printf("errfile: %s\n", stderr_filepath);
 	printf("max_len: %i\n", max_len);
 	printf("\n");
+	*/
+
+	int i; i=0;
+	printf("before for....\n");
+	for(i = 0; i < curr_args; i++){
+		printf("args[%i] --> code: %i | ", i, args[i].arg_code);
+		if(args[i].arg_code < S_ARG_MAXLEN){
+			printf("%s \n", args[i].arg_value);
+		}
+		else{
+			printf("%i \n", *(int*)args[i].arg_value);
+		}
+	}
 
 	//reading commands expressions from stdin logic
 	char input[CMD_EXP_BUFF_SIZE];
